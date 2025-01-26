5d48d1877d2fefb942b7ae9062edc4b24edac08d
Moritz Birghan <mb@mbirghan.com>
2025-01-26 13:09:28 +0100

    Remove photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -181,34 +181,6 @@ function Resume() {
   )
 }
 
-// TODO: Change photos
-function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
-
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-              rotations[imageIndex % rotations.length],
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
 export default async function Home() {
   return (
     <>
@@ -242,8 +214,6 @@ export default async function Home() {
           </div>
         </div>
       </Container>
-      {/* TODO: I want this in because it looks nice but don't know what to do with it right now */}
-      {/* <Photos /> */}
       {/* TODO: Add GitHub activity here */}
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
